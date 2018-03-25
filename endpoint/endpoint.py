f675126cadac6d960aae639ea5a95c68990ec1a4
Qingqing Li <qingqingli@Qingqings-MacBook-Pro.local>
2018-03-24 19:18:52 -0500

    private registry works, endpoint notification failed

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -1,11 +1,15 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/check_image', methods=['POST'])
+def check_image():
+    data = json.loads(request.data)
+    print('success')
+    return 'Done'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
