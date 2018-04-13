d856de2fb476a0abbe7f97331e669ffdc1703de9
Qingqing Li <liqingqing@tamu.edu>
2018-04-13 15:42:07 -0500

    connect with global check

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -2,6 +2,8 @@
 from flask import Flask, request
 import json
 import utils
+import os
+import global_check
 
 app = Flask(__name__)
 CORS(app)
@@ -12,9 +14,19 @@ def check_image():
 
     paths = utils.get_image_name(data)
     for p in paths:
-	utils.pull_image(p)
-	utils.save_image(p)
-	utils.untar_image(p)
+        utils.pull_image(p)
+        utils.save_image(p)
+        utils.untar_image(p)
+
+        file_lst = []
+        for root, dirs, files in os.walk('./test'):
+            for name in files:
+                print(os.path.join(root, name))
+                file_lst.append(os.path.join(root, name))
+        num_bad_files, suspicious_file_paths_list = global_check.global_process_files(file_lst)
+        print('-------------------')
+        print('num_bad_files: %d' %(num_bad_files))
+        print('suspicious_file_paths_list: %s' %(suspicious_file_paths_list))
     return 'Done'
 
 @app.route('/')
