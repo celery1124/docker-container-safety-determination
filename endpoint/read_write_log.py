d3a983939ba7f5567fb73636ae624602bfd610db
skintang <tsjtux@gmail.com>
2018-04-21 14:38:42 -0500

    add log funcs

--- endpoint/read_write_log.py
+++ endpoint/read_write_log.py
@@ -3,13 +3,14 @@ LOG_FILE = "registry-log.txt"
 
 
 def write_log(p, num_bad_files, suspicious_file_paths_list):
-    line = str(datetime.now()) + " " + p
+    line = "<p>" + str(datetime.now()) + "; Image: " + p
     if num_bad_files == 0:
-        line +=  "OK\n"
+        line +=  "; OK\n</p>"
     else:
-        line += " Suspicious with files: \n"
+        line += "; Suspicious with files: </p><ul>"
         for suspicious_file_path in suspicious_file_paths_list:
-            line += "\t%s\n" % suspicious_file_path 
+            line += "<li>%s</li>" % suspicious_file_path 
+        line += "</ul>"
     with open(LOG_FILE  , 'a') as file:
         file.write(line)
 
@@ -22,7 +23,6 @@ def test():
     p = ["d1", "d2", "d3"]
     num_bad_files = [1, 0, 2]
     suspicious_file_paths_list = [["path/f1"], [], ["path2", "path333"]]
-    write_log(p[0], num_bad_files[0], suspicious_file_paths_list[0])
     for i in range(0, 3):
         write_log(p[i], num_bad_files[i], suspicious_file_paths_list[i])
     print(read_log())
